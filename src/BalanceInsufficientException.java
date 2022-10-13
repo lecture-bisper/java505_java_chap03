dea4177cd32ca021bab9e9cc54d0f97773987425
lecture-bisper <bisper@outlook.kr>
2022-10-13 15:58:33 +0900

    1. 사용자 정의 예외 클래스 만들어 사용하기. 2. readme 파일 수정.

---
+++ src/BalanceInsufficientException.java
@@ -0,0 +1,7 @@
+public class BalanceInsufficientException extends Exception {
+    public BalanceInsufficientException() {}
+
+    public BalanceInsufficientException(String message) {
+        super(message);
+    }
+}
